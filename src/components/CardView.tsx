9b5bb5b6f47bf2d01ae6ad0b5e4c1e7a7655fb65
AntoniStachowski <antonistachowski@gmail.com>
2023-02-02 23:41:38 +0100

    Added prices and legalities to card view

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -7,9 +7,16 @@ import { requestPath } from "../utils";
 import NavBar from "./NavBar";
 import { ImageAspectRatio } from "@mui/icons-material";
 import Stack from '@mui/material/Stack';
-import { makeStyles } from "@mui/styles";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { idText } from "typescript";
 
-const useStyles = makeStyles({
+const classes = {
     leftGridItem: {
         fontSize: 20,
         minHeight: 40,
@@ -21,12 +28,36 @@ const useStyles = makeStyles({
     rightGridItem: {
         fontSize: 20,
     }
-})
+};
+
 
 function CardView() {
     const [image, setImage] = useState("");
     const [card, setCard] = useState<any>();
     const [manaObj, setManaString] = useState<any>();
+    function createPricesData(
+        type: string,
+        eur: string,
+        usd: string,
+        tix: string
+        ) {
+        return { type, eur, usd, tix};
+        }
+    
+    
+    const [costs, setCosts] = useState(
+        [createPricesData('Non-foil', "-","-", "-"),
+        createPricesData('Foil', "-", "-", "-")]
+    );
+
+    function createLegalityData(
+        format: string,
+        legality: string
+    ) {
+        return {format, legality}
+    }
+
+    const [legalities, setLegalities] = useState([createLegalityData("Standard", "Illegal")]);
 
     //todo - można wyeksportować do jakichś utilsów
     const stringManaToArray = (manaString: []) => {
@@ -54,6 +85,8 @@ function CardView() {
         setManaString(stringManaToArray(cardFromLocalStorage.mana_cost))
         console.log("xd")
 
+
+        // TODO - poprawić
         fetch(`${requestPath}/images/?id=${cardFromLocalStorage.id}`, {
             method: 'GET',
         }).then((response) => {
@@ -65,17 +98,134 @@ function CardView() {
             response.json().then((data) => {
                 setImage(data.normal);
             });
+
+            fetchPrices(cardFromLocalStorage.id);
+            fetchLegalities(cardFromLocalStorage.id);
         });
+    }
+
+    const fetchPrices = async (id: number) => {
+        const prices = await fetch(`${requestPath}/prices/?id=${id}`, {
+            method: 'GET',
+        });
+
+        if(!prices.ok) {
+            console.log("Ceny skopane");
+            return;
+        }
+
+        const numberOrNull = (value: any) => {
+            console.log(value);
+            return value == null ? "-" : value; 
+        }
+
+        const pricesJson = (await prices.json())[0];
+
+        console.log(pricesJson);
+
+        const pricesTable = [createPricesData("Non-foil", numberOrNull(pricesJson.eur),  numberOrNull(pricesJson.usd), numberOrNull(pricesJson.tix)),
+        createPricesData("Foil", numberOrNull(pricesJson.eur_foil),  numberOrNull(pricesJson.usd_foil), numberOrNull(null))];
+        
+        console.log(pricesTable);
+
+        setCosts(pricesTable);
+
+        console.log("Ceny wrzucone");
+    }
+
+    const fetchLegalities = async (id: number) => {
+        const legalities = await fetch(`${requestPath}/legalities/?id=${id}`, {
+            method: 'GET',
+        });
+
+        if(!legalities.ok) {
+            return;
+        }
+
+        const prettyString = (s: string) => {
+            return s == "legal" ? "Legal" : "Not legal";
+        }
+
+        const legalitiesJson = await legalities.json();
+
+        const legalitiesTable = [createLegalityData("Standard", prettyString(legalitiesJson.standard)),
+        createLegalityData("Pioneer", prettyString(legalitiesJson.pioneer)),
+        createLegalityData("Modern", prettyString(legalitiesJson.modern)),
+        createLegalityData("Legacy", prettyString(legalitiesJson.legacy)),
+        createLegalityData("Vintage", prettyString(legalitiesJson.vintage)),
+        createLegalityData("Commander", prettyString(legalitiesJson.commander)),
+        ];
+
+        setLegalities(legalitiesTable);
     }
 
     useEffect(() => {
         console.log("gowno")
         fetchCardData();
-    }, [])
+    }, []);
+    
+    function CostsTable() {
+    return (
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead>
+            <TableRow>
+                <TableCell ><Typography fontWeight={600}>Prices</Typography></TableCell>
+                <TableCell align="right"><Typography fontWeight={600}>EUR</Typography></TableCell>
+                <TableCell align="right"><Typography fontWeight={600}>USD</Typography></TableCell>
+                <TableCell align="right"><Typography fontWeight={600}>TIX</Typography></TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {costs.map((row) => (
+                <TableRow
+                key={row.type}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                    {row.type}
+                </TableCell>
+                <TableCell align="right">{row.eur}</TableCell>
+                <TableCell align="right">{row.usd}</TableCell>
+                <TableCell align="right">{row.tix}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    );
+    }
+
+    function LegalityTable() {
+        return (
+            <TableContainer component={Paper}>
+            <Table size="small" aria-label="a dense table">
+                <TableHead>
+                <TableRow>
+                    <TableCell><Typography fontWeight={600}>Format</Typography></TableCell>
+                    <TableCell align="right"><Typography fontWeight={600}>Legality</Typography></TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {legalities.map((row) => (
+                    <TableRow
+                    key={row.format}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell component="th" scope="row">
+                        {row.format}
+                    </TableCell>
+                    <TableCell align="right">{row.legality}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        );
+        }
+
 
     const left = 3;
-
-    const classes = useStyles(); 
 
     return (
         <div style = {{padding: 20, display: "flex"}}>
@@ -89,16 +239,16 @@ function CardView() {
                         Card summary
                     </Typography>
                     <Grid container>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Name
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem}>
+                        <Grid item xs = {12 - left} sx = {classes.rightGridItem}>
                             {(card) ? (card.card_name) : "card name"}
                         </Grid>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Mana cost
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem}>
+                        <Grid item xs = {12 - left} sx = {classes.rightGridItem}>
                         {!manaObj ? "gowno" :
                         (manaObj.cost === '')
                             ? ''
@@ -116,38 +266,38 @@ function CardView() {
                             />
                         ))}
                         </Grid>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Type line
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem}>
+                        <Grid item xs = {12 - left} sx = {classes.rightGridItem}>
                             {(card) ? (card.type_line) : "card type line"}
                         </Grid>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Power/toughness
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem}>
+                        <Grid item xs = {12 - left} sx = {classes.rightGridItem}>
                             {(card) ? `${card.power}/${card.toughness}` : "card stats"}
                         </Grid>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Text
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem}>
+                        <Grid item xs = {12 - left} sx = {classes.rightGridItem}>
                             {(card) ? (card.card_text) : "card text"}
                         </Grid>
-                        <Grid item xs = {left} className = {classes.leftGridItem}>
+                        <Grid item xs = {left} sx = {classes.leftGridItem}>
                             Flavor text
                         </Grid>
-                        <Grid item xs = {12 - left} className = {classes.rightGridItem} sx = {{fontStyle: "italic"}}>
+                        <Grid item xs = {12 - left} sx = {[classes.rightGridItem, {fontStyle: "italic"}]}>
                             {(card) ? (card.flavor_text) : "card flavor text"}
                         </Grid>
                     </Grid>
                 </div>
                 <div style = {{display: "flex"}}>
                     <div style = {{width: "100%", backgroundColor: "red", margin: 40}}>
-                        jakaś tabelka na koszty
+                        {CostsTable()};
                     </div>
-                    <div style = {{width: "100%", backgroundColor: "blue", margin: 40}}>
-                        jakaś tabelka na dostępność
+                    <div style = {{width: "100%", backgroundColor: "blue", margin: 40, paddingRight: 300}}>
+                        {LegalityTable()};
                     </div>
                 </div>
             </div>
