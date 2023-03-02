56de026deb0244f8138a7c319f1ba6ce83f76b59
Michał Kostyk <m.kostyk22@gmail.com>
2023-03-02 13:53:00 +0100

    Updated homepage for hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-      <HashRouter basename="/mtg-deck-builder">
+      <HashRouter>
         <App />
       </HashRouter>
     </React.StrictMode>
